74a3bce1b32f2fd0bd57984845385156210e2e5b
Christopher McFarland <christopherdmcfarland@gmail.com>
2021-08-10 16:06:41 -0400

    refactoring

--- histogram/histogram.c
+++ histogram/histogram.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 
-#define TRIALS          	pow(10,9)
-#define XMAX            	2
-#define BINS            	2000
+#define TRIALS          pow(10,9)
+#define XMAX            2
+#define BINS            2000
 #define FILENAME		"counts.csv"
 
 #ifdef EXPONENTIAL
@@ -11,41 +11,35 @@
 #define SETUP()			exponential_setup()
 #define GENERATOR()		exponential()
 #define NAME			"Exponential (Modified Ziggurat)"
-#define XMIN            	0
+#define XMIN            0
 #endif
 #ifdef NORMAL
 #include "../normal.h"
 #define SETUP()			normal_setup()
 #define GENERATOR()		normal()
 #define NAME			"Standard Normal (Modified Ziggurat)"
-#define XMIN            	-XMAX
+#define XMIN            -XMAX
 #endif
 
 int main( int argc, const char* argv[] ) {
     SETUP();
 
-    // The intervals are the lower limit of each bin.
-    double interval[BINS+1];
-    int observed[BINS];
-    for (int i = 0; i < BINS; i++) {
-        interval[i] = XMIN + (XMAX - XMIN) * (i + 1.0) / BINS;
-        observed[i] = 0;
-    }
-   
-    // Create histogram
+        // Create histogram
+    int observed[BINS] = {0};
     for (int i = 0; i < TRIALS; i++) {
         double x = GENERATOR();
         if (x >= XMIN && x < XMAX) {
-            	int index = (int)(BINS*(x - XMIN)/(XMAX - XMIN));
-		observed[index]++;
-	}
-    }
+            int index = (int)(BINS*(x - XMIN)/(XMAX - XMIN));
+            observed[index]++;
+        }
 
-    // Save the histogram
+        // Save the histogram
     FILE* f = fopen(FILENAME, "w");
     fprintf(f, "Low,High,%s\n", NAME);
     for (int i = 0; i < BINS; i++) {
-	fprintf(f, "%.16e,%.16e,%d\n", interval[i], interval[i+1], observed[i]);
+        double low = XMIN + (XMAX - XMIN) * i / BINS;
+        double high = XMIN + (XMAX - XMIN) * (i+1) / BINS;
+	    fprintf(f, "%.16e,%.16e,%d\n", low, high, observed[i]);
     }
     fclose(f);
 }
